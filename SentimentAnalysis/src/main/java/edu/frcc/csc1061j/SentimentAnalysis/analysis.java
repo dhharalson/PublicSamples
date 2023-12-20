8ee40830355cf5d059d52d7e887ed19591b64447
dhharalson <123024219+dhharalson@users.noreply.github.com>
2023-12-20 09:55:25 -0700

    Add files via upload

---
+++ SentimentAnalysis/src/main/java/edu/frcc/csc1061j/SentimentAnalysis/analysis.java
@@ -0,0 +1,15 @@
+package edu.frcc.csc1061j.SentimentAnalysis;
+
+public class analysis {
+
+	public static void loadSentiments() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public static void printSentiments() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
